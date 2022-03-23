aa5e65741581dcb783bed9d2de14acec0071fe4f
raulmatrix <101440848+raulmatrix@users.noreply.github.com>
2022-03-23 01:23:20 -0400

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,35 @@
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 15;
+        int numero = 100 + 5;
+        int numero2 = num1+num2;
+        
+        double numero3 = 10.0/20.0;
+        
+        System.out.println("El resultado de la division: "+numero3);
+        
+        
+        int residuo = 20%14;
+        System.out.println("El residuo es: "+residuo);
+        
+        //incremento y decremento
+        
+        int valorx = 1;
+        int valory = 100;
+        
+        ++valorx;
+        
+        System.out.println("Incremento X: "+valorx);
+        --valory;
+        System.out.println("Decremento Y; "+valory);
+        
+        //operador de asignacion
+        int w = 5;
+        w +=4; //w = 5+4
+        
+    }
+    
+}
