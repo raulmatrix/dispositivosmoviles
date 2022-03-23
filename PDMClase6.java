b618fd0b5a2e606bc2bd691fcae3f412bdf15df0
raulmatrix <101440848+raulmatrix@users.noreply.github.com>
2022-03-23 01:22:15 -0400

    Create PDMClase6.java

---
+++ PDMClase6.java
@@ -0,0 +1,47 @@
+
+package pdmclase6;
+
+public class PDMClase6 {
+
+    public static void main(String[] args) {
+        
+        String mes="enero";
+        /*
+        switch(mes){
+            case "enero":
+                System.out.println("Enero encontrado");
+                break;
+            case "febrero":
+                System.out.println("Febrero encontrado");
+                break;
+            case "marzo":
+                System.out.println("Marzo encontrado");
+                break;
+            default:
+                System.out.println("El mes no fue encontrado");
+                
+        }*/
+        
+        //estructura while
+        /*int i = 0;
+        while (i <= 5) {
+          System.out.println(i);
+          i++;
+        }*/
+        
+        /*int j=2;
+        int limite=20;
+        while(j<limite){
+            System.out.println(j);
+            j=j+2;
+        }*/
+        
+        //for
+        
+        for(int i=0;i<=20;i = i + 2){
+            System.out.print(i+"-");
+        }
+        
+    }
+    
+}
